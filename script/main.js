21f423a46eb0f58e5ec9390e6c09ffd4228c1f70
gusbernabe <gusbernabe.gb@gmail.com>
2023-04-19 19:36:50 -0300

    PreEntrega2+Bernabe

--- script/main.js
+++ script/main.js
@@ -1,41 +1,42 @@
 // INICIO PRE-ENTREGA 1
 // Sumar totales de productos seleccionados y calcular descuento segun promocion de segunda unidad en ofertas.html
-// let cantidad = parseInt(prompt("Ingresa la cantidad de conjuntos que deseas adquirir:"));
-// let precioConjunto = 15000;
-// let porcentaje = 15;
-// let descuento = 0;
-// let total = 0;
 
-// if (cantidad >= 2) {
-//   total = cantidad * precioConjunto;
-//   descuento = total * (porcentaje/100);
-//   total = total - descuento;
-//   alert("Debido a la cantidad de conjuntos que deseas adquirir obtienes un 15% de descuento, el precio final es de $" + total);
-//   console.log("Debido a la cantidad de conjuntos que deseas adquirir obtienes un 15% de descuento, el precio final es de $" + total);
-// } else {
-//   total = cantidad * precioConjunto;
-//   alert("El total por la cantidad que deseas adquirir es $" + total)
-//   console.log("El total por la cantidad que deseas adquirir es $" + total);
-// }
+let cantidad = parseInt(prompt("Ingresa la cantidad de conjuntos que deseas adquirir:"));
+let precioConjunto = 15000;
+let porcentaje = 15;
+let descuento = 0;
+let total = 0;
+
+if (cantidad >= 2) {
+  total = cantidad * precioConjunto;
+  descuento = total * (porcentaje / 100);
+  total = total - descuento;
+  alert("Debido a la cantidad de conjuntos que deseas adquirir obtienes un 15% de descuento, el precio final es de $" + total);
+  console.log("Debido a la cantidad de conjuntos que deseas adquirir obtienes un 15% de descuento, el precio final es de $" + total);
+} else {
+  total = cantidad * precioConjunto;
+  alert("El total por la cantidad que deseas adquirir es $" + total)
+  console.log("El total por la cantidad que deseas adquirir es $" + total);
+}
 
 // Calcular la cantidad de unidades en stock restante segun la cantidad que vaya a adquirir el usuario 
 // El valor de la variante cantidad se debe restar del total del stock 
-// let stock = 20;
-// let unidadesAReducir = cantidad;
+let stock = 20;
+let unidadesAReducir = cantidad;
 
-// for (let i = stock; i > 0; i--) {
-//   if (i >= unidadesAReducir) {
-//     stock = stock - unidadesAReducir;
-//     if (stock == 1) {
-//       console.log("Queda 1 unidad en stock");
-//     } else {
-//       console.log("Quedan " + stock + " unidades en stock");
-//     }
-//     break;
-//   } else {
-//     console.log("No hay unidades suficientes en stock");
-//   }
-// }
+for (let i = stock; i > 0; i--) {
+  if (i >= unidadesAReducir) {
+    stock = stock - unidadesAReducir;
+    if (stock == 1) {
+      console.log("Queda 1 unidad en stock");
+    } else {
+      console.log("Quedan " + stock + " unidades en stock");
+    }
+    break;
+  } else {
+    console.log("No hay unidades suficientes en stock");
+  }
+}
 
 // FIN PRE-ENTREGA 1
 
@@ -54,7 +55,6 @@ class Producto {
   };
 }
 
-// Supuesta cantidad de productos seleccionados: 6
 let misProductos = [
   new Producto(1, "Conjunto Monocrome", 15000),
   new Producto(2, "Conjunto Military Green", 15000),
@@ -64,14 +64,22 @@ let misProductos = [
   new Producto(6, "Conjunto Blue", 15000),
 ];
 
-let precioTotal = misProductos.reduce(
-  (acumulador, unProducto) => acumulador + unProducto.precio,
-  0
+// Econtrar productos con un filter
+
+let productoBuscado = prompt("¿Qué productos deseas buscar?");
+
+let productosHallados = misProductos.filter(
+  (unProducto) => unProducto.nombre.includes(productoBuscado.toUpperCase())
 );
 
-console.log(
-  "--> El total por la cantidad de conjuntos seleccionados es " + precioTotal.toFixed(2)
-);
+if (productosHallados.length > 0) {
+  let nombresProductosHallados = productosHallados.map((unProducto) => unProducto.nombre);
+  let mensaje = "La cantidad de productos que se hallaron según tu búsqueda " + productoBuscado + " son: " + productosHallados.length + ". Los nombres de los productos son: " + nombresProductosHallados.join(", ");
+
+  alert(mensaje);
+} else {
+  alert("Lo siento, no se encontraron productos que coincidan con tu búsqueda.");
+}
 
 // Calcular una fecha mínima de días para retirar un producto teniendo en cuenta el día que fue adquirido
 
